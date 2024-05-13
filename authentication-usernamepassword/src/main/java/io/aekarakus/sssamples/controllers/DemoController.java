c8e6b9b4f02a7c1133b0b86df453ae3934a01b3d
aekarakus <alperenkarakus@aol.com>
2024-05-13 15:59:33 +0300

    added restcontroller

---
+++ authentication-usernamepassword/src/main/java/io/aekarakus/sssamples/controllers/DemoController.java
@@ -0,0 +1,13 @@
+package io.aekarakus.sssamples.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+}
