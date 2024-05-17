3d1efdf6cd27750ff88f77639bb2eddefdc347fb
aekarakus <alperenkarakus@aol.com>
2024-05-17 19:24:25 +0300

    chore: added freeipa context

---
+++ ldapclient/src/main/java/io/aekarakus/sssamples/ldapclient/config/IPAServerConfig.java
@@ -0,0 +1,27 @@
+package io.aekarakus.sssamples.ldapclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+@Configuration
+public class IPAServerConfig {
+
+    @Bean
+    ContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource("ldap://192.168.60.221:389/dc=springframework,dc=org");
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(BaseLdapPathContextSource contextSource) {
+        LdapPasswordComparisonAuthenticationManagerFactory factory = new LdapPasswordComparisonAuthenticationManagerFactory(contextSource, NoOpPasswordEncoder.getInstance());
+        factory.setUserDnPatterns("uid={0},ou=people");
+        factory.setPasswordAttribute("userPassword");
+        return factory.createAuthenticationManager();
+    }
+}
