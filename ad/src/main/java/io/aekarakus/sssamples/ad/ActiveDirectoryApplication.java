9178fe3aa2b9bfea0476cba5f50ed26d329a5fbe
aekarakus <alperenkarakus@aol.com>
2024-05-20 14:07:47 +0300

    chore: added ad context

---
+++ ad/src/main/java/io/aekarakus/sssamples/ad/ActiveDirectoryApplication.java
@@ -0,0 +1,17 @@
+package io.aekarakus.sssamples.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class ActiveDirectoryApplication
+{
+    public static void main( String[] args ){
+        SpringApplication.run(ActiveDirectoryApplication.class, args);
+    }
+}
