b0ed9184730a6eaf2da5c55a11ed306bd98869d9
aekarakus <alperenkarakus@aol.com>
2024-05-20 17:36:22 +0300

    fix: ad

--- ad/src/main/java/io/aekarakus/sssamples/ad/config/SecurityConfig.java
+++ ad/src/main/java/io/aekarakus/sssamples/ad/config/SecurityConfig.java
@@ -2,7 +2,12 @@ package io.aekarakus.sssamples.ad.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 
 @Configuration
 public class SecurityConfig {
