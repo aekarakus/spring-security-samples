01375135148bef8c9b3f3fb849ed569862b729e1
aekarakus <alperenkarakus@aol.com>
2024-05-21 18:02:49 +0300

    chore: changed module name

---
+++ userdetails/src/main/java/io/aekarakus/sssamples/userdetails/JDBCAuthorizationApplication.java
@@ -0,0 +1,16 @@
+package io.aekarakus.sssamples.userdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class JDBCAuthorizationApplication
+{
+    public static void main( String[] args ){
+        SpringApplication.run(JDBCAuthorizationApplication.class, args);
+    }
+}
---
+++ userdetails/src/main/java/io/aekarakus/sssamples/userdetails/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.aekarakus.sssamples.userdetails.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    ContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource("ldap://localhost:33389/dc=springframework,dc=org");
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(BaseLdapPathContextSource contextSource) {
+        LdapPasswordComparisonAuthenticationManagerFactory factory = new LdapPasswordComparisonAuthenticationManagerFactory(contextSource, NoOpPasswordEncoder.getInstance());
+        factory.setUserDnPatterns("uid={0},ou=people");
+        factory.setPasswordAttribute("userPassword");
+        return factory.createAuthenticationManager();
+    }
+}
