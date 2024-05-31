59e50b945673bfe5f7a1e3bb7e4f734a53f2c9cc
aekarakus <alperenkarakus@aol.com>
2024-05-31 10:18:41 +0300

    feat: codeflow sub-oauth2 project done

--- oauth/codeflow/oauth2codeflowresourceserver/src/main/java/io/aekarakus/oauth2codeflowresourceserver/config/SecurityConfig.java
+++ oauth/codeflow/oauth2codeflowresourceserver/src/main/java/io/aekarakus/oauth2codeflowresourceserver/config/SecurityConfig.java
@@ -1,7 +1,25 @@
 package io.aekarakus.oauth2codeflowresourceserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth
+                        .anyRequest().authenticated()
+        ).oauth2ResourceServer(
+                rs -> rs
+                        .jwt(
+                                jwt -> jwt.jwkSetUri("http://localhost:5025/oauth2/jwks")
+                        )
+        );
+        return http.build();
+    }
 }
